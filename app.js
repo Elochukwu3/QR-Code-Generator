33089b08cbbccfe3edbb85a690b0ec0ac9e436d2
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-03 11:44:51 +0100

    brancjh saved

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-
-const innerWrapper =
-        ` <h2>QR Code Reader/Generator</h2>
+const innerWrapper = ` <h2>QR Code Generator</h2>
         <input type="text" id="text" placeholder="Enter text ..." class="input" />
         <button type="button"  id="button">Generate QR</button>
         <div class="qr-image">
           <span class="error"></span><br />
           <img src="" id="img" />
         </div>
-`
+`;
 document.querySelector(".page-container").innerHTML = innerWrapper;
 function generateQR() {
   document.querySelector(".qr-image").style.display = "block";
   let QRtext = document.querySelector("#text").value;
-  
+
   if (QRtext == 0) {
     document.querySelector(".qr-image .error").innerHTML = "Please enter text";
     document.querySelector("#img").style.display = "none";
   } else {
     document.querySelector("#img").style.display = "block";
     document.querySelector(".qr-image .error").innerHTML = "";
-    document.querySelector("#img").src =
-      `https://api.qrserver.com/v1/create-qr-code/?data=${QRtext}&amp;size=100x100`;
-      
+    document.querySelector(
+      "#img"
+    ).src = `https://api.qrserver.com/v1/create-qr-code/?data=${QRtext}&amp;size=100x100`;
   }
 }
-document.getElementById("button").addEventListener("click", generateQR)
+document.getElementById("button").addEventListener("click", generateQR);
